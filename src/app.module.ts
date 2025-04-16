72e9242d5c0b6708d6adb223bff1f7914a2dc45b
German Bustos Moreno <germanthegamer1@outlook.com>
2025-04-16 00:31:02 -0600

    se hizo la conexion de la bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { SongModule } from './song/song.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'coritos_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,  
+      synchronize: false, // No uses true en producción
     }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
